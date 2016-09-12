9759c689520f4f23497dad3cc197b98a1b38c67a
Benjamin Robson <unoceptik@gmail.com>
2016-09-12 19:31:26 +0300

    Added some fun code

--- grave5.py
+++ grave5.py
@@ -1,11 +1,14 @@
 #Backslash escapes quotes that are using within the string
 print "I am \"sarcastic\" often."
 
-#"\t"
+# "\t" = tab
 cat = "\t I'm tabbed in."
 persian_cat = "I'm split\non a line."
+
+#This allows the usage of backslash
 backslash_cat = "I'm \\ a \\ cat."
 
+#Creates a tabbed list
 fat_cat = """
 I'll do a list:
 \t* cat food
@@ -17,3 +20,14 @@ print cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+#Fun piece of little code
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i
+
+#Some more fun
+for i in ["Ben", "harry", "high"]:
+    print "%s \n" % i
+else:
+    print "Nothing"
