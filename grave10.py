431d5fc416f6ddae543b753161e13c7347fc13fd
Benjamin Robson <unoceptik@gmail.com>
2016-09-21 09:01:25 +0300

    Added: added string formatting

--- grave10.py
+++ grave10.py
@@ -2,15 +2,18 @@ from sys import argv
 
 script, filename = argv
 
+#Asks user if they want file to be deleted
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print "If you don't want that, hit RETURN."
 
 raw_input("?")
 
+#Opens file
 print "Opening the file..."
 target = open(filename, 'w')
 
+#Deletes (truncates) file
 print "Truncating the file. Goodbye!"
 target.truncate()
 
@@ -22,12 +25,8 @@ line3 = raw_input("Line3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
+#User raw_input from lines 22-24 put into txt file
+target.write('%s \n %s \n %s \n' % (line1,line2,line3))
+#Most importantly, closing the file
 print "And finally, we close it."
 target.close()
---
+++ grave9_sample.py
@@ -0,0 +1,3 @@
+Benjamin 
+ is 
+ water 
