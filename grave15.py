38d55c98a12b6580df422e293ea38b75ab6b9bfd
Benjamin Robson <unoceptik@gmail.com>
2016-10-07 11:33:32 +0300

    Using functions to create variables

---
+++ grave15.py
@@ -0,0 +1,37 @@
+#Returning functions and using functions to set variables
+#Adding function
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+#Subtracting function
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+#Multiplying function
+def multiply(a ,b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+#Dividing function
+def divide(a ,b):
+    print "MULTIPLYING %d / %d" % (a, b)
+    return a / b
+
+print "Let's do some math with just functions!"
+
+#Calls functions and sets variables
+age = add(30,5)
+height = subtract(78,4)
+weight = multiply(10,10)
+iq = divide(100,2)
+
+#Prints variables with string formatters
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+#Using variables in functions
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+#Prints above variable
+print "That becomes: ", what, "Can do you do it by hand?"
