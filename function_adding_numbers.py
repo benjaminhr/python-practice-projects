4483df76aea9f5f46500b6aaaf06d03e37c61308
Benjamin Robson <unoceptik@gmail.com>
2016-10-07 10:37:51 +0300

    File reading functions

--- function_adding_numbers.py
+++ function_adding_numbers.py
@@ -1,47 +1,70 @@
 import sys
+import os
 
 start = ""
 def main(start):
-    #Asks the user if they would like to add two numbers
-    print """\n
-    -----------------------------------------------
-    Welcome to the addition and multiplication bot!
-    ----------------------------------------------- \n
-    """
+    #while loop used for restarting script
+    while True:
+        #Asks the user if they would like to add two numbers
+        print """\n
+        -----------------------------------------------
+        Welcome to the addition and multiplication bot!
+        ----------------------------------------------- \n
+        """
 
-    print "Would you like to add two numbers together? Hit RETURN."
-    print "If you would not like to do so, CTRL-C."
-    raw_input("> ")
+        print "Would you like to add two numbers together?"
+        print "Press RETURN to continue, or CTRL-C to cancel."
+        raw_input("> ")
 
-    #Asks for numbers and adds to variable
-    numbers1 = int(raw_input('Number 1: '))
-    numbers2 = int(raw_input('Number 2: '))
+        #Asks for numbers and adds to variable
+        numbers1 = int(raw_input('Number 1: '))
+        numbers2 = int(raw_input('Number 2: '))
 
-    #Function takes two numbers and adds them
-    def addition(numb1, numb2):
-        answer = numbers1 + numbers2
-        #Prints the result
-        print "%r + %r = %r \n" % (numb1, numb2, answer)
+        #Function takes two numbers and adds them
+        def addition(numb1, numb2):
+            answer = numbers1 + numbers2
+            #Prints the result
+            print "%r + %r = %r \n" % (numb1, numb2, answer)
 
-    #Calls the function
-    addition(numbers1, numbers2)
+        #Calls the function
+        addition(numbers1, numbers2)
 
-    #Asks for multiplication
-    answer_2 = numbers1 + numbers2
-    print 'Would you like to multiply your previous result: %r?' % answer_2
-    print 'RETURN to continue, CTRL-C to cancel.'
-    raw_input("> ")
+        #Asks for multiplication
+        answer_2 = numbers1 + numbers2
+        print 'Would you like to multiply your previous result: %r?' % answer_2
+        print 'RETURN to continue, CTRL-C to cancel.'
+        raw_input("> ")
 
-    #Creates variable for number
-    numbers3 = int(raw_input('What number would you like to multiply by: '))
+        #Creates variable for number
+        numbers3 = int(raw_input('What number would you like to multiply by: '))
 
-    #Function for multiplication
-    def multiplication(numb3):
-        answer_mult = answer_2 * numbers3
+        #Function for multiplication
+        def multiplication(numb3):
+            answer_mult = answer_2 * numbers3
 
-        #Prints result
-        print "%r x %r = %r \n" % (answer_2, numbers3, answer_mult)
-        print "Thank you for using my addition and multiplication bot! \n"
-    multiplication(numbers3)
+            #Prints result
+            print "%r x %r = %r \n" % (answer_2, numbers3, answer_mult)
+            print "Thank you for using my addition and multiplication bot! \n"
+        multiplication(numbers3)
 
+        #Asks user if they want to restart script
+        print "Would you like to restart?"
+        print "Y for yes, N for no."
+
+        #Collects response
+        option = str(raw_input("> "))
+
+        #Function for restarting using 'execfile' and 'sys.exit'
+        def restart(option):
+            if option == "Y":
+                print "Loading... "
+                #Clears CLI
+                os.system("clear")
+                #Re-runs script
+                execfile("function_adding_numbers.py")
+            else:
+                #Exits program
+                sys.exit("Program closing... 0")
+        restart(option)
+#Calls function main
 main(start)
---
+++ grave14.py
@@ -0,0 +1,39 @@
+from sys import argv
+
+#Creates input for user --> file
+script, input_file = argv
+
+#Read all contents
+def print_all(f):
+    print f.read()
+
+#Read invidivual contents
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now lets rewind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print all lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
